03477aa7fe8147b2bae4f05ebf483192f6507254
fahimahammed <fahimahammed.cse@gmail.com>
2023-01-14 20:44:10 +0600

    deleting data

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -68,5 +68,10 @@ exports.Mutation = {
         db.products = db.products.filter(product => product.id !== id);
         db.reviews = db.reviews.filter(review => review.productId !== id);
         return true;
+    },
+    deleteReview: (parent, {id}, {db})=>{
+        db.reviews = db.reviews.filter(review => review.id !== id);
+        // db.products = db.
+        return true;
     }
 }
